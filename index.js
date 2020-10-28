400fdf6fa90396d8b6cc55409a0179436328df61
tj10200 <>
2020-10-28 14:08:03 -0600

    exporting the new objects

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = {
   Encoder: require('./lib/encoder.js'),
   Decoder: require('./lib/decoder.js'),
+  Reencoder: require('./lib/reencoder.js'),
+  Utils: require('./lib/utils.js'),
   libopus: require('./build/libopus.js').instance
 };
